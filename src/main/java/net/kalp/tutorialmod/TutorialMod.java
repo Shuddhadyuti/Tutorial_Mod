88fe00aeb020bbd5a1e8bd1d96bf73298e9c238a
Shuddhadyuti <shuddhadyuti.sarkar@gmail.com>
2023-10-04 15:27:06 +0530

    Initial Commit

--- src/main/java/net/kalp/tutorialmod/TutorialMod.java
+++ src/main/java/net/kalp/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.kalp.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kalp.tutorialmod.item.ModItemGroups;
+import net.kalp.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
