1f06427ed51c51a9ffa4b381f7977806a13ef82a
Shuddhadyuti <shuddhadyuti.sarkar@gmail.com>
2023-10-04 14:25:17 +0530

    Initial commit

---
+++ src/main/java/net/kalp/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.kalp.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
